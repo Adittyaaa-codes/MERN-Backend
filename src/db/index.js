49e85a1c614cacbf1d6527f0a547500fba8a191d
Adittyaaa-codes <aditya.mallick.1512@gmail.com>
2025-10-30 19:21:27 +0530

    database connected successfully

---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async()=>{
+    try {
+        const connectionInstance = mongoose.connect(`${process.env.DATABASE_URL}/${DB_NAME}`);
+        console.log("MongoDB connected");
+        console.log(connectionInstance);
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+export default connectDB;
