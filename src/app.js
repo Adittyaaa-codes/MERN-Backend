c74310cd7f610210222d3e58edc03c8ce4a77efc
Adittyaaa-codes <aditya.mallick.1512@gmail.com>
2026-01-24 18:55:40 +0530

    feat(cors): Add 'X-Requested-With' to allowed headers for CORS

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.use(cors({
   credentials: true,
   optionsSuccessStatus: 200,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
   exposedHeaders: ['set-cookie']
 }));
 
